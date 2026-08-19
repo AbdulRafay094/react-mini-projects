7bd9ed147064da87d0d749ebc472ed0f9399db1d
Abdul Rafay <iamabdrafay@gmail.com>
2026-08-19 09:51:27 +0500

    .

--- UI_website/src/components/Section1/HeroText.jsx
+++ UI_website/src/components/Section1/HeroText.jsx
@@ -6,7 +6,7 @@ function HeroText() {
       <h3 className="mb-8 text-[4.1rem] leading-[0.9] font-bold tracking-[-0.08em] text-[#1b1b1b]">
         Prospective <br /> customer <br /> segmentation
       </h3>
-      <p className="text-[1.15rem] leading-[1.5] w-[85%] font-medium text-[#4b4b4b]">
+      <p className="text-[1.15rem] leading-normal w-[85%] font-medium text-[#4b4b4b]">
         Depending on customer satisfaction and access to banking products,
         potential target audience can be divided into three groups.
       </p>
